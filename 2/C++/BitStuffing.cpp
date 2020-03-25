1aaf18e084a94cc83fb4a1f6ed189f54d3f1d007
Skybluewater <Skybluewater@users.noreply.github.com>
2020-03-26 02:22:18 +0800

    3.1.2

---
+++ 2/C++/BitStuffing.cpp
@@ -0,0 +1,107 @@
+//
+//  main.cpp
+//  BitStuffing
+//
+//  Created by 崔程远 on 2020/3/25.
+//  Copyright © 2020 qc. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    string k,a,b;
+    b=a="01111110";
+    int flag=0;
+    cout<<"帧起始标志: "<<b<<" 帧数据信息: ";
+    for(int i=0;i<32;i++){
+        char temp;
+        cin>>temp;
+        k+=temp;
+        if(temp=='1'){
+            flag++;
+            if(flag==5){
+                k.append("0");
+                flag=0;
+            }
+        }else{
+            flag=0;
+        }
+    }
+    a.append(k);
+    a.append(b);
+    cout<<endl<<"比特填充后发送帧: "<<a<<endl;
+    int n=0;
+    int num=0,start=0,bfor=0;
+    flag=0;
+    string s;
+    for(int i=0;i<a.length();i++){
+        if(a[i]=='0'){
+            if(bfor==1&&num>0){
+                bfor=1;
+                s+="0";
+                flag=1;
+                start=i;
+            }else if(n<5&&num>0&&n>0){
+                bfor=1;
+                flag=1;
+                s.append(a,start,i-start);
+                n=0;
+                start=i;
+            }else if(n==5&&num>0){
+                flag=0;
+                bfor=0;
+                if(i+1>=a.length()){
+                    cout<<"传输出错"<<endl;
+                    break;
+                }
+                s.append(a,start,i-start);
+                n=0;
+                start=i+1;
+            }else if(n>5&&num>0){
+                cout<<"传输出错"<<endl;
+                break;
+            }else{
+                bfor=1;
+                flag=1;
+                start=i;
+            }
+        }else{
+            bfor=0;
+            if(flag==1){
+                n++;
+                if(n==6){
+                    if(i+1<a.length()){
+                        if(a[i+1]=='0'){
+                            if(++num==2){
+                                break;
+                            }
+                            n=0;
+                            flag=0;
+                            i++;
+                            start=i+1;
+                        }else{
+                            cout<<"传输出错"<<endl;
+                            break;
+                        }
+                    }else{
+                        cout<<"error transmission"<<endl;
+                        break;
+                    }
+                }
+            }else if(flag==0){
+                n++;
+                if(n>5){
+                    cout<<"error transmission"<<endl;
+                    break;
+                }
+            }
+        }
+    }
+    cout<<"比特删除的帧数据: "<<s<<endl;
+    return 0;
+}
---
+++ 2/C++/ByteStuffing.cpp
@@ -0,0 +1,92 @@
+//
+//  main.cpp
+//  Bytestuffing
+//
+//  Created by 崔程远 on 2020/3/26.
+//  Copyright © 2020 qc. All rights reserved.
+//
+
+#include <iostream>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    char k[8][3];
+    memset(k,0,sizeof(k));
+    string a="7E",b="7E",c;
+    cout<<"帧起始标志: "<<b<<" 帧数据信息: ";
+    for(int i=0;i<16;i++){
+        cin>>k[i/2][i%2];
+    }
+    char m[20][3];
+    memset(m,0,sizeof(m));
+    int j=0;
+    for(int i=0;i<8;i++){
+        if(k[i][0]=='7'&&k[i][1]=='E'){
+            m[j][0]='7';m[j][1]='D';
+            m[++j][0]='5';m[j][1]='E';
+            j++;
+        }
+        else if(k[i][0]=='7'&&k[i][1]=='D'){
+            m[j][0]='7';m[j][1]='D';
+            m[++j][0]='5';m[j][1]='D';
+            j++;
+        }
+        else if(k[i][0]<'2'){
+            m[j][0]='7';m[j][1]='D';
+            m[++j][0]=k[i][0]+2;m[j][1]=k[i][1];
+            j++;
+        }else{
+            m[j][0]=k[i][0];m[j][1]=k[i][1];
+            j++;
+        }
+    }
+    for(int i=0;i<j;i++){
+        c=c+m[i][0]+m[i][1];
+    }
+    a.append(c);
+    a.append(b);
+    cout<<"帧结束标志: "<<b<<" 发送帧: "<<a;
+    int num=0;
+    string l;
+    for(int i=0;i<a.length();){
+        if(a[i]=='7'&&a[i+1]=='E'){
+            num++;
+            if(num>=2){
+                break;
+            }
+            i+=2;
+        } else if(a[i]=='7'&&a[i+1]=='D'&&num>0){
+            i+=2;
+            if(i>a.length()){
+                cout<<"error"<<endl;
+                break;
+            }
+            if(a[i]=='5'&&a[i+1]=='D'){
+                l.append("7D");
+            }else if(a[i]=='5'&&a[i+1]=='E'){
+                l.append("7E");
+            }else{
+                if(a[i]>'3'){
+                    cout<<"error"<<endl;
+                    break;
+                }else{
+                    l+=a[i]-2;
+                    l+=a[i+1];
+                }
+            }
+            i=i+2;
+        } else if(num>0){
+            l+=a[i];
+            l+=a[i+1];
+            i+=2;
+        }
+    }
+    if(num<2){
+        cout<<"error"<<endl;
+    }
+    cout<<endl<<"删除后的接收帧: "<<l<<endl;
+    return 0;
+}
