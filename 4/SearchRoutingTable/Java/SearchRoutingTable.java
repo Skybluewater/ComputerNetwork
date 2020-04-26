c1b3c4a91faf992fb2c2aca735bc10037355368d
surpass <975266353@qq.com>
2020-04-26 11:34:05 +0800

    老街

---
+++ 4/SearchRoutingTable/Java/SearchRoutingTable.java
@@ -0,0 +1,97 @@
+
+/*
+ https://blog.csdn.net/qq_34134278/article/details/83538898
+ https://blog.csdn.net/gatieme/article/details/50989257?depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-1&utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-1
+ https://blog.csdn.net/qq_34902437/article/details/91346938
+ Book Page: 340-345
+ */
+import java.util.Scanner;
+
+public class SearchRoutingTable {
+	private static int n = 3;
+	private int IPAddress[][] = new int[n][4];
+	private int subnetMask[][] = new int[n][4];
+	private String nextHop[] = new String[] { "Interface0", "Interface1", "Router1", "Router2" };
+
+	private int IPSubAddress[][] = new int[][] { { 135, 46, 56, 0 }, { 136, 46, 60, 0 }, { 192, 53, 40, 0 } };
+	private int prefix[] = new int[] { 22, 22, 23 };
+
+	public void Initialize() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 4; j++) {
+				IPAddress[i][j] = Integer.parseInt(Integer.toBinaryString(IPSubAddress[i][j]));
+			}
+			String string = "";
+			for (int j = 0; j < prefix[i]; j++) {
+				string += "1";
+			}
+			for (int j = prefix[i]; j < 32; j++) {
+				string += "0";
+			}
+			for (int j = 0; j < 4; j++) {
+				subnetMask[i][j] = Integer.parseInt(string.substring(j * 8, j * 8 + 8));
+			}
+		}
+	}
+
+	public void printRoutingTable() {
+		System.out.println("Routing Table:");
+		System.out.println("IPAddress/Prefix\tNextHop");
+		for (int i = 0; i < n + 1; i++) {
+			if (i == n) {
+				System.out.printf("0/0\t\t\t");
+			} else {
+				System.out.printf("%d", IPSubAddress[i][0]);
+				for (int j = 1; j < 4; j++) {
+					System.out.printf(".%d", IPSubAddress[i][j]);
+				}
+				System.out.printf("/%d\t\t", prefix[i]);
+			}
+			System.out.println(nextHop[i]);
+		}
+		System.out.println();
+	}
+
+	public void Search(String string) {
+		String subIPAddress[] = string.split("\\.");
+		int maxPrefixPos = n;
+		int maxPrefixLen = 0;
+		for (int i = 0; i < n; i++) {
+			boolean flag = true;
+			for (int j = 0; j < 4; j++) {
+				int value1 = Integer.parseInt(String.valueOf(subnetMask[i][j]), 2);
+				int value2 = Integer.parseInt(subIPAddress[j]);
+				int value3 = Integer.parseInt(String.valueOf(IPAddress[i][j]), 2);
+				if ((value1 & value2) != value3) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == false) {
+				System.out.printf("Searching line %d, not matched.\n", i);
+			} else if (flag == true) {
+				System.out.printf("Searching line %d, matched successfully.\n", i);
+				if (prefix[i] > maxPrefixLen) {
+					maxPrefixLen = prefix[i];
+					maxPrefixPos = i;
+				}
+			}
+
+		}
+		System.out.println("Next hop: " + nextHop[maxPrefixPos]);
+	}
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(
+				"Please input Destination IPAddress of datagram as format 'num1.num2.num3.num4' such as '192.53.41.128'(no ''):");
+		String string = scanner.nextLine();
+		System.out.println();
+		SearchRoutingTable operation = new SearchRoutingTable();
+		operation.Initialize();
+		operation.printRoutingTable();
+		operation.Search(string);
+	}
+
+}
---
+++ 4/SearchRoutingTable/Python/SearchRoutingTable.py
@@ -0,0 +1,66 @@
+class SearchRoutingTable:
+    n = 3
+    IPAddress = [[0] * 4 for i in range(3)]
+    subnetMask = [[0] * 4 for i in range(3)]
+    nextHop = ["Interface0", "Interface1", "Router1", "Router2"]
+
+    IPSubAddress = [[135, 46, 56, 0], [136, 46, 60, 0], [192, 53, 40, 0]]
+    prefix = [22, 22, 23]
+
+    def Initialize(self):
+        for i in range(self.n):
+            for j in range(4):
+                self.IPAddress[i][j] = int(bin(self.IPSubAddress[i][j]).replace('0b', ''))
+            string = ""
+            for j in range(self.prefix[i]):
+                string += "1"
+            for j in range(self.prefix[i], 32):
+                string += "0"
+            for j in range(4):
+                self.subnetMask[i][j] = int(string[j * 8: j * 8 + 8])
+
+    def printRoutingTable(self):
+        print("Routing Table:")
+        print("IPAddress/Prefix\tNextHop")
+        for i in range(self.n + 1):
+            if i == self.n:
+                print("0/0\t\t\t\t\t", end='')
+            else:
+                print("%d" % self.IPSubAddress[i][0], end='')
+                for j in range(1, 4):
+                    print(".%d" % self.IPSubAddress[i][j], end='')
+                print("/%d\t\t" % self.prefix[i], end='')
+            print(self.nextHop[i])
+        print()
+
+    def Search(self, string):
+        subIPAddress = string.split('.')
+        maxPrefixPos = self.n
+        maxPrefixLen = 0
+        for i in range(self.n):
+            flag = True
+            for j in range(4):
+                value1 = int(str(self.subnetMask[i][j]), 2)
+                value2 = int(subIPAddress[j])
+                value3 = int(str(self.IPAddress[i][j]), 2)
+                if ((value1 & value2) != value3):
+                    flag = False
+                    break
+            if flag == False:
+                print("Searching line %d, not matched." % i)
+            elif flag == True:
+                print("Searching line %d, matched successfully." % i)
+                if self.prefix[i] > maxPrefixLen:
+                    maxPrefixLen = self.prefix[i]
+                    maxPrefixPos = i
+        print("Next hop: " + self.nextHop[maxPrefixPos])
+
+
+if __name__ == '__main__':
+    string = input(
+        "Please input Destination IPAddress of datagram as format 'num1.num2.num3.num4' such as '192.53.41.128'(no ''):\n")
+    print()
+    operation = SearchRoutingTable()
+    operation.Initialize()
+    operation.printRoutingTable()
+    operation.Search(string)
